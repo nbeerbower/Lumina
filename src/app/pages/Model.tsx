dc093b4b3e688633f9666bcaf891748225a46eb5
Nicholas Beerbower <nbeerbower@gmail.com>
2024-02-18 21:38:31 -0500

    model page skeleton

--- src/app/pages/Model.tsx
+++ src/app/pages/Model.tsx
@@ -1,7 +1,30 @@
+import { useRef, useState, useEffect } from 'react'
+
+// TODO: break into components and style
+
 function Model() {
+	const fileInput = useRef(null)
+	const [fileIsSelected, setFileIsSelected] = useState(false)
+	const [modelPath, setModelPath] = useState(null)
+
+	const handleLoad = () => {
+		const file = fileInput.current.files[0]
+		setModelPath(file.path)
+		console.log(file)
+	}
+
+	const handleClear = () => {
+		fileInput.current.value = ''
+		setFileIsSelected(false)
+	}
+
 	return (
 		<div>
 			<h1>Model</h1>
+			<p>Select a GGUF model from the disk.</p>
+			<input type="file" accept=".gguf" ref={fileInput} onChange={() => setFileIsSelected(fileInput.current.files.length > 0)} />
+			<button onClick={handleLoad} disabled={!fileIsSelected}>Load</button>
+			<button onClick={handleClear} disabled={!fileIsSelected}>Clear</button>
 		</div>
 	)
 }
